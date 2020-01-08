b9066d3d198bad7bfa9d480d5f413505b4e60478
Rizquha Maulida <Rizquha.Maulida@bsi.co.id>
2020-01-08 10:45:55 +0700

    README.md

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Siapa Namamu ?");
+            string username = Console.ReadLine();
+            Console.WriteLine("Nama saya adalah "+username);
+        }
+    }
+}
